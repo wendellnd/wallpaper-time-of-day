59c9779e6eb10654fef8d97bc2486e39e8059fb2
wendellnd <wendelldourado.wn@gmail.com>
2023-09-26 22:39:00 -0300

    refac

--- extension.js
+++ extension.js
@@ -3,64 +3,55 @@ const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 const Gio = imports.gi.Gio;
 
-let panelButton, panelButtonText;
-let backgroundSetting, screensaverSetting;
+let settings;
 let timeout;
 
-function init() {
-  panelButton = new St.Bin({
-    style_class: "panel-button",
-  });
+function getURI() {
+  const hour = new Date().getHours();
 
-  icon = new St.Icon({
-    icon_name: "popup-menu-icon",
-    style_class: "system-status-icon",
-  });
+  const morning = hour > 6 && hour < 12;
+  const noon = hour >= 12 && hour < 17;
+  const sundown = hour >= 17 && hour < 20;
 
-  panelButton.set_child(icon);
+  uri = "file:///home/wendellnd/Imagens/onepiece-wallpapers";
+
+  if (morning) {
+    uri = uri + "/morning.jpg";
+  } else if (noon) {
+    uri = uri + "/noon.jpg";
+  } else if (sundown) {
+    uri = uri + "/sundown.jpg";
+  } else {
+    uri = uri + "/evening.jpg";
+  }
+
+  return uri;
 }
 
-function enable() {
-  Main.panel._rightBox.insert_child_at_index(panelButton, 1);
-
-  timeout = Mainloop.timeout_add_seconds(1, () => {
-    const hour = new Date().getHours();
-
-    const morning = hour > 6 && hour < 12;
-    const noon = hour >= 12 && hour < 17;
-    const sundown = hour >= 17 && hour < 20;
-
-    uri = "file:///home/wendellnd/Imagens/onepiece-wallpapers";
-
-    if (morning) {
-      uri = uri + "/morning.jpg";
-    } else if (noon) {
-      uri = uri + "/noon.jpg";
-    } else if (sundown) {
-      uri = uri + "/sundown.jpg";
-    } else {
-      uri = uri + "/evening.jpg";
-    }
-
-    backgroundSetting = new Gio.Settings({
-      schema: "org.gnome.desktop.background",
-    });
-    backgroundSetting.set_string("picture-uri", uri);
-    backgroundSetting.set_string("picture-uri-dark", uri);
-
-    screensaverSetting = new Gio.Settings({
-      schema: "org.gnome.desktop.screensaver",
-    });
-    screensaverSetting.set_string("picture-uri", uri);
-    screensaverSetting.set_string("picture-uri-dark", uri);
-
-    Gio.Settings.sync();
+function setBackground(schema, uri) {
+  settings = new Gio.Settings({
+    schema: schema,
   });
+  settings.set_string("picture-uri", uri);
+  settings.set_string("picture-uri-dark", uri);
+}
+
+function handleMainLoop() {
+  uri = getURI();
+
+  setBackground("org.gnome.desktop.background", uri);
+  setBackground("org.gnome.desktop.screensaver", uri);
+
+  Gio.Settings.sync();
+}
+
+function init() {}
+
+function enable() {
+  timeout = Mainloop.timeout_add_seconds(1, handleMainLoop);
 }
 
 function disable() {
   Mainloop.source_remove(timeout);
-  Main.panel._rightBox.remove_child(panelButton);
-  backgroundSetting = null;
-  screensaverSetting = null;
+  settings = null;
 }
